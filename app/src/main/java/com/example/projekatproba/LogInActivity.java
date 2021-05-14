97ddafc1cebbe859df9330f3db3a2a1c7ded394a
17375JanaRajkovic <jana.rajkovic@elfak.rs>
2021-05-14 22:39:12 +0200

    Log in i home, Jana

---
+++ app/src/main/java/com/example/projekatproba/LogInActivity.java
@@ -0,0 +1,38 @@
+package com.example.projekatproba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LogInActivity extends AppCompatActivity {
+
+    TextView forgot;
+    TextView register;
+    Button login;
+    EditText username;
+    EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        forgot=findViewById(R.id.forgot);
+        password=findViewById(R.id.password);
+        register=findViewById(R.id.register);
+        username= findViewById(R.id.username);
+        login=findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),HomeActivity.class));
+            }
+        });
+    }
+}
