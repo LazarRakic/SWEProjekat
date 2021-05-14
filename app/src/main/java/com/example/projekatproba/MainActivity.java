64d89fef8d256f243a1d582c32f8224c73bd5e26
jovan17196 <jovann.krstic@elfak.rs>
2021-05-14 22:53:59 +0200

    Jovan deo I

--- app/src/main/java/com/example/projekatproba/MainActivity.java
+++ app/src/main/java/com/example/projekatproba/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.projekatproba;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        androidx.constraintlayout.widget.ConstraintLayout relativeLayoutMain = (androidx.constraintlayout.widget.ConstraintLayout) findViewById(R.id.constraintlay);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+        alphaAnimation.setDuration(3000);
+        alphaAnimation.setFillAfter(true);
+        relativeLayoutMain.startAnimation(alphaAnimation);
     }
 }
+
