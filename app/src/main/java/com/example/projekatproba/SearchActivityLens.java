fd49c827d33f6cce8384440219ee442ffd0be589
jovan17196 <jovann.krstic@elfak.rs>
2021-05-24 23:02:57 +0200

    SearchActivity

---
+++ app/src/main/java/com/example/projekatproba/SearchActivityLens.java
@@ -0,0 +1,14 @@
+package com.example.projekatproba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchActivityLens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_lens);
+    }
+}
