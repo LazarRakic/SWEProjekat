84d8159805307e2f7a003ce3c3862b41ec89d8a7
jovan17196 <jovann.krstic@elfak.rs>
2021-06-13 17:47:58 +0200

    bruh

--- app/src/main/java/com/example/projekatproba/SearchActivity.java
+++ app/src/main/java/com/example/projekatproba/SearchActivity.java
@@ -221,7 +221,7 @@ public class SearchActivity extends AppCompatActivity {
                         }
 
                     }
-                });*/
+                });
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
